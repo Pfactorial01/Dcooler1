73fb8993bb37109ee8cff8dd9047e48131a3e98e
Pfactorial01 <adeoyeadebayo18@gmail.com>
2022-03-08 09:54:38 +0100

    modified:   Dcooler1/settings.py 	modified:   Project_Flow

--- Dcooler1/settings.py
+++ Dcooler1/settings.py
@@ -33,15 +33,14 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
-    'Project_Flow',
-    
+INSTALLED_APPS = [     
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Project_Flow',
 ]
 
 MIDDLEWARE = [
