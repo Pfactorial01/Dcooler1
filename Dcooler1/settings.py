6dec7425cd4f284cef154dad7eece1352e5f64e6
Adebayo Adeoye <71315247+Pfactorial01@users.noreply.github.com>
2022-03-09 07:15:36 +0100

    Update settings.py

--- Dcooler1/settings.py
+++ Dcooler1/settings.py
@@ -122,6 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
