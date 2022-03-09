d48dc62b05852ce3845c71ff5e2d5c74145b5e49
Adebayo Adeoye <71315247+Pfactorial01@users.noreply.github.com>
2022-03-09 06:32:43 +0100

    Update admin.py

--- Project_Flow/admin.py
+++ Project_Flow/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Issue, Personnel, Project
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+#from .models import User
 
 # Register your models here.
 
